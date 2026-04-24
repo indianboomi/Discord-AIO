dbf73fe0d7344f4f8d50b40a8b2e51bab42557d9
indianboomi <indianboomi157@gmail.com>
2026-04-24 14:28:31 +0530

    Update smpstatus.js

--- commands/smpstatus.js
+++ commands/smpstatus.js
@@ -1,1 +1,18 @@
+const util = require('minecraft-server-util');
 
+module.exports = {
+    name: 'smpstatus',
+    async execute(message) {
+        try {
+            const res = await util.status(process.env.MC_HOST, {
+                port: 25565
+            });
+
+            message.channel.send(
+                `🟢 SMP Online\n👥 Players: ${res.players.online}/${res.players.max}`
+            );
+        } catch {
+            message.channel.send('🔴 SMP Offline');
+        }
+    }
+};
